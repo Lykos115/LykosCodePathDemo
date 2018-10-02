a2fd6c5f1dfe224cf9580e3fd69e6e0a7543e07e
Lykos115 <javierherrera52@gmail.com>
2018-10-01 17:48:00 -0700

    Additional features

--- FranciscoCodePathDemo/FranciscoCodePathDemo/ViewController.swift
+++ FranciscoCodePathDemo/FranciscoCodePathDemo/ViewController.swift
@@ -11,8 +11,13 @@ import UIKit
 class ViewController: UIViewController {
 
     @IBOutlet weak var textLabel: UILabel!
+    
+    var backgroundColor: UIColor!
+    var orginaltextColor: UIColor!
     override func viewDidLoad() {
         super.viewDidLoad()
+        orginaltextColor = textLabel.textColor
+        backgroundColor = view.backgroundColor
         // Do any additional setup after loading the view, typically from a nib.
     }
 
@@ -20,6 +25,35 @@ class ViewController: UIViewController {
         print("hello");
         textLabel.textColor = UIColor.red;
     }
+    
+    @IBAction func didTapViewButton(_ sender: Any) {
+        view.backgroundColor = UIColor.blue;
+        
+    }
+    
+    
+    @IBAction func didTapTextButton(_ sender: Any) {
+//        textLabel.text = "Goodbye!";
+        textLabel.text = textField.text;
+        textField.text = "";
+        view.endEditing(true);
+    }
+    
+    
+    
+    @IBAction func onResetGesture(_ sender: Any) {
+        textLabel.text = "Hello";
+        textLabel.textColor = orginaltextColor;
+        view.backgroundColor = backgroundColor;
+    }
+    
+    
+    
+    @IBAction func textField(_ sender: Any) {
+    }
+    
+    @IBOutlet weak var textField: UITextField!
+    
     
 }
 
